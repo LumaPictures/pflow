672580229336a254fc67bd96e09b2b4c587c5b75
Chris Lyon <chrisl@luma-pictures.com>
2015-08-03 20:01:40 -0700

    disabled __all__ in __init__.py so that setup.py works

--- pflow/__init__.py
+++ pflow/__init__.py
@@ -1,9 +1,13 @@
 __version__ = '0.1.0'
 
-__all__ = [
-    'core',
-    'components',
-    'exc'
-]
+# TODO: need a better way of managing the version from a single place.
+# setup.py references __version__ from this file, but including sub-modules
+# will cause setup.py to fail because of unmet dependencies.
 
-from . import core, components, exc
+# __all__ = [
+#     'core',
+#     'components',
+#     'exc'
+# ]
+#
+# from . import core, components, exc
